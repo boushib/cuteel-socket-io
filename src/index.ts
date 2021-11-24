c0d4ddae00298ffccfe582f620b5e260bb571722
boushib <boushib01@gmail.com>
2021-11-24 19:16:10 +0100

    use redis to save notifications

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,51 @@ import express, { Request, Response } from 'express'
 import { config } from 'dotenv'
 import { createServer } from 'http'
 import { Server as IOServer } from 'socket.io'
+import { createClient } from 'redis'
+import { promisify } from 'util'
+import { Notification } from './models'
 
 config()
 
 const app = express()
 const server = createServer(app)
-const io = new IOServer(server)
+const ioOptions: any = { cors: true }
+const io = new IOServer(server, ioOptions)
+
+const redisClient = createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  //password: process.env.REDIS_PASSWORD,
+})
+
+const redisGetAsync = promisify(redisClient.get).bind(redisClient)
+
+const saveNotification = async (notification: Notification) => {
+  const notificationsHistory = await redisGetAsync('notifications')
+  let notifications: Array<any> = []
+
+  if (notificationsHistory) {
+    notifications = JSON.parse(notificationsHistory)
+  }
+
+  notifications.push(notification)
+  redisClient.set('notifications', JSON.stringify(notifications))
+}
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello!' })
 })
 
 io.on('connection', (socket) => {
+  socket.on('joinRoom', async (room: string) => {
+    const roomHistory = await redisGetAsync(room)
+    if (!roomHistory) return
+    const history = JSON.parse(roomHistory)
+    socket.emit('roomHistory', history)
+  })
+
   socket.on('notification', (notification) => {
+    saveNotification(notification)
     io.emit('notification', notification)
   })
 })
